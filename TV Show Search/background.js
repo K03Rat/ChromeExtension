6805886f7d376a6431cc24a2a3606b45eabb25b7
Kristian Ratnikov <kristianratnikov28@gmail.com>
2024-02-19 23:50:01 +0000

    Content script, more APIs messsage parsing, data fetching and http requests

--- TV Show Search/background.js
+++ TV Show Search/background.js
@@ -1,14 +1,45 @@
+// Add a listener for when the extension is installed or updated
 chrome.runtime.onInstalled.addListener((details) => {
-  chrome.contextMenus.create({
-    title: "Test Contest Menu",
-    id: "contexMenu1",
-    contexts: ["page", "selection"],
+  // When installed or updated, set an initial empty array for the 'shows' key in local storage
+  chrome.storage.local.set({
+    shows: [],
   });
-});
-chrome.contextMenus.onClicked.addListener((event) => {
-  chrome.tabs.create({
-    url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm`,
-  });
-});
 
-console.log("background scrioptt running");
+  // Create a context menu item for searching TV shows
+  chrome.contextMenus.create({
+    title: "Search TV Show",
+    id: "contextMenu1",
+    contexts: ["page", "selection"], // Show this context menu item when selecting text on a page
+  });
+
+  // Create a context menu item for reading selected text
+  chrome.contextMenus.create({
+    title: "Read This Text",
+    id: "contextMenu2",
+    contexts: ["page", "selection"], // Show this context menu item when selecting text on a page
+  });
+
+  // Add a listener for when a context menu item is clicked
+  chrome.contextMenus.onClicked.addListener((event) => {
+    // Check which context menu item was clicked
+    if (event.menuItemId === "contextMenu1") {
+      // If 'Search TV Show' context menu item was clicked
+      // Fetch TV show data based on the selected text using TVMaze API
+      fetch(`http://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+        .then((response) => response.json()) // Parse the JSON response
+        .then((data) => {
+          console.log(data); // Log the fetched data
+          // Store the fetched data in local storage under the 'shows' key
+          chrome.storage.local.set({
+            shows: data,
+          });
+        });
+    } else if (event.menuItemId === "contextMenu2") {
+      // If 'Read This Text' context menu item was clicked
+      // Use text-to-speech API to read the selected text
+      chrome.tts.speak(event.selectionText, {
+        rate: 0.5, // Set the rate of speech
+      });
+    }
+  });
+});
